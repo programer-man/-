60d8d2eab795cd3a53fbd3d37b3236928cd627b3
zhangtom <raegenrg@gmail.com>
2023-06-25 08:47:00 +0800

    15首页模块-左侧分类-骨架效果

---
+++ src/components/library/index.js
@@ -0,0 +1,14 @@
+// 扩展vue原有的功能：全局组件，自定义指令，挂载原型方法
+// 插件
+// vue2 插件写法要素： 导出一个对象，有install函数，默认传入了Vue构造函数，Vue基础之上扩展
+// vue3 : 导出一个对象，有install函数，默认传入了app应用实例,app基础之上扩展
+
+import XtxSkeleton from '../library/xtx-skeleton.vue'
+
+export default {
+  install(app) {
+    // 在app上进行扩展，app提供component directive函数
+    // 如果要挂在原型 app.config.globalProperties方式
+    app.component(XtxSkeleton.name, XtxSkeleton)
+  }
+}
--- src/views/home/components/home-category.vue
+++ src/views/home/components/home-category.vue
@@ -6,6 +6,10 @@
         <template v-if="item.children">
           <RouterLink v-for="sub in item.children" :key="sub.id" :to="`/category/sub/${sub.id}`">{{ sub.name }}</RouterLink>
         </template>
+        <span v-else>
+          <XtxSkeleton width="60px" height="18px" style="margin-right: 5px" bg="rgba(255,255,255,0.2)" />
+          <XtxSkeleton width="50px" height="18px" bg="rgba(255,255,255,0.2)" />
+        </span>
       </li>
     </ul>
     <!-- 弹层 -->
@@ -204,6 +208,17 @@ export default {
     .layer {
       display: block;
     }
+  }
+}
+.xtx-skeleton {
+  animation: fade 1s linear infinite alternate;
+}
+@keyframes fade {
+  from {
+    opacity: 0.2;
+  }
+  to {
+    opacity: 1;
   }
 }
 </style>
