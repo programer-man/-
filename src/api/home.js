48e9f5d66b662bba5c09eed7ca18b2b4585cf248
zhangtom <raegenrg@gmail.com>
2023-06-25 09:20:39 +0800

    17首页模块-轮播图-渲染结构

--- src/api/home.js
+++ src/api/home.js
@@ -3,3 +3,8 @@ import request from '@/utils/request'
 export const findBrand = (limit) => {
   return request('/home/brand', 'get', { limit })
 }
+
+// 获取广告图
+export const findBanner = () => {
+  return request('/home/banner', 'get')
+}
--- src/views/home/components/home-banner.vue
+++ src/views/home/components/home-banner.vue
@@ -1,11 +1,20 @@
 <template>
   <div class="home-banner">
-    <XtxCarousel />
+    <XtxCarousel  :sliders="sliders"/>
   </div>
 </template>
 <script>
+import { findBanner } from '@/api/home'
+import { ref } from 'vue'
 export default {
-  name: 'HomeBanner'
+  name: 'HomeBanner',
+  setup() {
+    const sliders = ref([])
+    findBanner().then((data) => {
+      sliders.value = data.result
+    })
+    return { sliders }
+  }
 }
 </script>
 <style scoped lang="less">
@@ -17,12 +26,12 @@ export default {
   top: 0;
   z-index: 98;
   .xtx-carousel {
-  ::v-deep .carousel-btn.prev {
-    left: 270px;
+    ::v-deep .carousel-btn.prev {
+      left: 270px;
+    }
+    ::v-deep .carousel-indicator {
+      padding-left: 250px;
+    }
   }
-  ::v-deep .carousel-indicator {
-    padding-left: 250px;
-  }
-}
 }
 </style>
