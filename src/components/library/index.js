9ef4e71b8b73f044a0c9535a8e6cd126c691f649
zhangtom <raegenrg@gmail.com>
2023-06-25 08:55:39 +0800

    16首页模块-轮播图-基础渲染

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -4,11 +4,13 @@
 // vue3 : 导出一个对象，有install函数，默认传入了app应用实例,app基础之上扩展
 
 import XtxSkeleton from '../library/xtx-skeleton.vue'
+import XtxCarousel from '../library/xtx-carousel.vue'
 
 export default {
   install(app) {
     // 在app上进行扩展，app提供component directive函数
     // 如果要挂在原型 app.config.globalProperties方式
     app.component(XtxSkeleton.name, XtxSkeleton)
+    app.component(XtxCarousel.name, XtxCarousel)
   }
 }
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -4,6 +4,8 @@
       <div class="container">
         <!-- 左侧分类 -->
         <HomeCategory />
+        <!-- 轮播图 -->
+        <HomeBanner />
       </div>
     </div>
   </div>
@@ -11,10 +13,12 @@
 
 <script>
 import HomeCategory from './components/home-category.vue'
+import HomeBanner from './components/home-banner.vue'
 export default {
   name: 'PageHome',
   components: {
-    HomeCategory
+    HomeCategory,
+    HomeBanner
   }
 }
 </script>
