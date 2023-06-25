48d051fca2232420ba38db4161b5c5f4429cd1ce
zhangtom <raegenrg@gmail.com>
2023-06-25 10:52:24 +0800

    19-首页模块-面板封装

--- src/components/library/index.js
+++ src/components/library/index.js
@@ -5,12 +5,13 @@
 
 import XtxSkeleton from '../library/xtx-skeleton.vue'
 import XtxCarousel from '../library/xtx-carousel.vue'
-
+import XtxMore from '../library/xtx-more.vue'
 export default {
   install(app) {
     // 在app上进行扩展，app提供component directive函数
     // 如果要挂在原型 app.config.globalProperties方式
     app.component(XtxSkeleton.name, XtxSkeleton)
     app.component(XtxCarousel.name, XtxCarousel)
+    app.component(XtxMore.name, XtxMore)
   }
 }
